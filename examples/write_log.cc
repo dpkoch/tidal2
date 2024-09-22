f16679e0de3c6daee7284efee103bec767005098
Daniel Koch <daniel.p.koch@gmail.com>
2024-09-21 23:53:23 -0400

    Rename and expand write log example

--- examples/write_log.cc
+++ examples/write_log.cc
@@ -9,17 +9,17 @@
 #include "tidal/tidal.hh"
 
 using Vec4 = Eigen::Vector4d;
-using Mat3 = Eigen::Matrix3f;
+using Mat2x3 = Eigen::Matrix<double, 2, 3>;
 
 int main() {
     tidal::Log log(std::filesystem::temp_directory_path() / "meh.bin");
-    auto stream1 = log.add_stream<int, bool, Vec4, Mat3, double>("stuff");
-    stream1->set_labels("int", "bool", "Vec4", "Mat3", "double");
-    stream1->log(4000, 42, true, Vec4(12., 4., -23., 5.), Mat3::Random(), 123.);
+    auto stream1 = log.add_stream<int, bool, Vec4, Mat2x3, double>("stuff");
+    stream1->set_labels("int", "bool", "Vec4", "Mat2x3", "double");
+    stream1->log(4000, 42, true, Vec4(12., 4., -23., 5.), Mat2x3::Zero(), 123.);
 
     auto stream2 = log.add_stream<uint8_t>("small stream");
 
-    stream1->log(4001, 12, false, Vec4::Ones(), Mat3::Zero(), -9000.);
+    stream1->log(4001, 12, false, Vec4::Ones(), (Mat2x3() << 1, 2, 3, 4, 5, 6).finished(), -9000.);
     stream2->log(12345, 29);
 
     return EXIT_SUCCESS;
