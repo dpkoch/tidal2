7d22894642fffb79485f71194456797f8fafce26
Daniel Koch <daniel.p.koch@gmail.com>
2024-09-22 00:02:45 -0400

    Examples cleanup

--- examples/write_log.cc
+++ examples/write_log.cc
@@ -14,7 +14,7 @@ using Mat2x3 = Eigen::Matrix<double, 2, 3>;
 int main() {
     tidal::Log log(std::filesystem::temp_directory_path() / "meh.bin");
     auto stream1 = log.add_stream<int, bool, Vec4, Mat2x3, double>("stuff");
-    stream1->set_labels("int", "bool", "Vec4", "Mat2x3", "double");
+    stream1->set_labels("my_ints", "my_bools", "vectors", "matrices", "my_doubles");
     stream1->log(4000, 42, true, Vec4(12., 4., -23., 5.), Mat2x3::Zero(), 123.);
 
     auto stream2 = log.add_stream<uint8_t>("small stream");
